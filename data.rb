45c95bf5855c1a87480181e269553f9fb9cd852c
MissDine <enidnyatichi1212@gmail.com>
2022-06-06 15:39:51 +0300

    Feature edit

--- data.rb
+++ data.rb
@@ -3,3 +3,17 @@
 # We can know the type of  data by .class for instance
 3.class
 head.class
+# intergers
+3
+# string-defined by either single or double quotes
+"Enid"
+# Creating a new string
+String.new("Hi gal")
+# Sring interpolation in ruby
+# Float
+2.7
+#Boolean
+true,false
+# symbol
+# Hash
+# Array
